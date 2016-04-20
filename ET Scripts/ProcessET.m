66dbcec72bda5b30dadffae6ff6ee62b46de3def
Dan <d.brady@bbk.ac.uk>
2016-04-20 19:02:38 +0100

    Fix for actual sim display
    Changed to account for and calculate proportions for actual stimulus
    display length

--- ET Scripts/ProcessET.m
+++ ET Scripts/ProcessET.m
@@ -1,16 +1,23 @@
 % New Script to analyse MEM tasks for ET tasks on the LonDownS Alzheimer's Disease in Down syndrome Study.
-% V0.4 - 20/04/16
+% V0.5 - 20/04/16
 % Dan Brady
 
 orig_path = fileparts(mfilename('fullpath'));                              % Record folder the script was run from
 addpath(orig_path)                                                         % Add that folder to path
+cd(orig_path)                                                              % Change to that folder for creation of output file
 
 filename     = 'Event_markers.txt';                                        % Set filename
-eventMarkers = importdata(filename);                                       % Load markers in file
+eventMarkers = readtable(filename);                                        % Load markers in file
 ADDS_ET_data = struct();                                                   % Prep main output variable
 
+% Add option box asking about analyses
+% Modify header based of analyses indicated
+
 output_FName = 'ADDS_ET_output.csv';                                       % Specify output filename
-header       = 'Participant, Event, Time, Total Samples, Total looking, Top Left, Top Right, Bottom Left, Bottom Right\n'; % Specify output header (This will need modifying!)
+header       = ['Participant, Event ID, Event start time,'...
+                'Time between Markers, Samples between markers,'...
+                'Trial length, Samples in trial,'...
+                'Total looking, Top Left, Top Right, Bottom Left, Bottom Right\n']; % Specify output header (This will need modifying!)
 fid          = fopen(output_FName, 'w');                                   % Create and open output data file
 fprintf(fid, header);                                                      % Write header to data file
                                                            
@@ -19,12 +26,13 @@ cd(path)                                                                   % Cha
 files   = dir('ADDS*');                                                    % Get all the ADDS files available
 folders = files([files.isdir]);                                            % Select only the directories
 
-[eventInd,~] = listdlg('ListString', eventMarkers,...
+[eventInd,~] = listdlg('ListString', eventMarkers.Event_name,...
                        'SelectionMode', 'multiple');                       % Ask participant to select which event markers to find
-eventsToFind = eventMarkers(eventInd);                                     % Put these markers into a variable
+eventsToFind = eventMarkers(eventInd,:);                                   % Put these markers into a variable
 
 if ~isempty(eventsToFind)                                                  % Check for user input
-    for folder_n = 1:length(folders)                                       % Loop through each participant
+    for folder_n = 1:size(folders,1)                                       % Loop through each participant
+        commandwindow
         fprintf('\nStarting %s\n\n', folders(folder_n).name)               % Starting message
         p_path = sprintf('%s/%s', path, folders(folder_n).name);           % Create path for participants folder
         ADDS_ET_data.(folders(folder_n).name) = struct();                  % Create fieldname for the participant in output data variable
@@ -37,7 +45,7 @@ if ~isempty(eventsToFind)                                                  % Che
             continue                                                       % Then skip to the next participant
         end
         
-        for file_n = 1:length(dataFilePaths)                               % Loop through the paths
+        for file_n = 1:size(dataFilePaths,1)                               % Loop through the paths
             load(dataFilePaths(file_n).name)                               % Load the .mat files: eventBuffer, mainBuffer & timeBuffer
         end % file_n
         
@@ -50,12 +58,12 @@ if ~isempty(eventsToFind)                                                  % Che
         
         clear *Buffer                                                      % Clear loaded variables
         
-        for eventsToFind_n = 1:length(eventsToFind)                        % For each of the specified markers
-            len = length(eventsToFind{eventsToFind_n});                    % Get number of characters in that marker
-            ind = strncmpi(eventsToFind{eventsToFind_n},...
+        for eventsToFind_n = 1:size(eventsToFind,1)                        % For each of the specified markers
+            len = length(eventsToFind.Event_name{eventsToFind_n});         % Get number of characters in that marker
+            ind = strncmpi(eventsToFind.Event_name{eventsToFind_n},...
                            allEvents(:,3),len);                            % Find the specified markers in the allEvents variable
             foundEvents = [allEvents(ind, 3:-1:2) cell(sum(ind),1)];       % Place matching events and times in variable, add blank third column
-            for foundEvent_n = 1:length(foundEvents(:,1))                       % For each matching event found
+            for foundEvent_n = 1:size(foundEvents(:,1),1)                  % For each matching event found
                 foundEvents{foundEvent_n,3} = ...
                     find(allData >= foundEvents{foundEvent_n,2},1);        % Find the index of the first sample recorded after that event and record in third column
             end % foundEvent_n
@@ -67,21 +75,30 @@ if ~isempty(eventsToFind)                                                  % Che
             end
             
             for eventStarts_n = 1:length(eventStarts)                      % For each trigger found
-                ADDS_ET_data.(folders(folder_n).name).(eventsToFind{eventsToFind_n})(eventStarts_n,1) =...
-                    double(foundEvents{eventStarts(eventStarts_n)+1,2} - foundEvents{eventStarts(eventStarts_n),2})/1000; % Calculate the time between the start and end triggers (ms)
+                ADDS_ET_data.(folders(folder_n).name).(eventsToFind.Event_name{eventsToFind_n})(eventStarts_n,1) =...
+                    (foundEvents{eventStarts(eventStarts_n)+1,2} - foundEvents{eventStarts(eventStarts_n),2})/1000; % Calculate the time between the start and end triggers (ms)
                 
-                ADDS_ET_data.(folders(folder_n).name).(eventsToFind{eventsToFind_n})(eventStarts_n,2) =...
+                ADDS_ET_data.(folders(folder_n).name).(eventsToFind.Event_name{eventsToFind_n})(eventStarts_n,2) =...
                     length(foundEvents{eventStarts(eventStarts_n),3}:foundEvents{eventStarts(eventStarts_n)+1,3}); % Calculate the number of samples in the epoch
                 
-                ADDS_ET_data.(folders(folder_n).name).(eventsToFind{eventsToFind_n})(eventStarts_n,3:7) =...
-                    func_quadrantsLT(allData(foundEvents{eventStarts(eventStarts_n),3}:foundEvents{eventStarts(eventStarts_n)+1,3},:)); % Calculate the looking time per quadrant
+                trial_end_ind = find(allData >= (foundEvents{eventStarts(eventStarts_n),2}+(eventsToFind.Event_length(eventsToFind_n)*1000)),1);
                 
-                fprintf(fid,'%s,%s,%s,%d%s\n',...                         % Write data to file
+                ADDS_ET_data.(folders(folder_n).name).(eventsToFind.Event_name{eventsToFind_n})(eventStarts_n,3) =...
+                    (allData(trial_end_ind,1)- foundEvents{eventStarts(eventStarts_n),2})/1000;
+                
+                ADDS_ET_data.(folders(folder_n).name).(eventsToFind.Event_name{eventsToFind_n})(eventStarts_n,4) =...
+                    length(foundEvents{eventStarts(eventStarts_n),3}:trial_end_ind);
+                
+                ADDS_ET_data.(folders(folder_n).name).(eventsToFind.Event_name{eventsToFind_n})(eventStarts_n,5:9) =...
+                    func_quadrantsLT(allData(foundEvents{eventStarts(eventStarts_n),3}:trial_end_ind,:)); % Calculate the looking time per quadrant
+                
+                One_string = sprintf('%d,', ADDS_ET_data.(folders(folder_n).name).(eventsToFind.Event_name{eventsToFind_n})(eventStarts_n,:));
+                
+                fprintf(fid,'%s,%s,%s,%s\n',...                       % Write data to file
                     folders(folder_n).name,...                             % Participant ID
-                    eventsToFind{eventsToFind_n},...                       % Event
-                    num2str(ADDS_ET_data.(folders(folder_n).name).(eventsToFind{eventsToFind_n})(eventStarts_n,1)),...         % Event epoch
-                    ADDS_ET_data.(folders(folder_n).name).(eventsToFind{eventsToFind_n})(eventStarts_n,2),...                  % Samples in the epoch
-                    sprintf(',%d', ADDS_ET_data.(folders(folder_n).name).(eventsToFind{eventsToFind_n})(eventStarts_n,3:7)));  % Looking time quadrant output
+                    eventsToFind.Event_name{eventsToFind_n},...            % Event ID
+                    num2str(foundEvents{eventStarts(eventStarts_n),2}, '%20d'),... % Event start time
+                    One_string(1:end-1)); 
             end
             
         end % eventsToFind_n
@@ -91,7 +108,6 @@ if ~isempty(eventsToFind)                                                  % Che
         %clearvars('-except', vars);                                       % Clean up workspace
         
     end % folder_n
-
 end
 fclose(fid);                                                               % Close the data file
 cd(orig_path)                                                              % Change back to the original file path
