5dd76d7eebdc872e9925450964473b529b4cdb77
Dan <d.brady@bbk.ac.uk>
2016-04-22 10:49:23 +0100

    Bug fixes for quadrantsLT and added subdir function

--- ET Scripts/func_quadrantsLT.m
+++ ET Scripts/func_quadrantsLT.m
@@ -2,11 +2,11 @@ function data_out = func_quadrantsLT(data_in)
 
 LXRX_LYRY = data_in(:,[9, 22, 10, 23]);                                    % Extract data for individual eyes from epoch
 
-LXRX_LYRY(LXRX_LYRY<-0.5|LXRX_LYRY>1.05) = NaN;                            % Remove samples where participant was not looking at the screen
+LXRX_LYRY(LXRX_LYRY<-0.05|LXRX_LYRY>1.05) = NaN;                           % Remove samples where participant was not looking at the screen
 
-eyeXY = [nanmean(LXRX_LYRY(:,1:2),2)'; nanmean(LXRX_LYRY(:,3:4),2)']';        % Calculate single coordinate for looking 
+eyeXY = [nanmean(LXRX_LYRY(:,1:2),2)'; nanmean(LXRX_LYRY(:,3:4),2)']';     % Calculate single coordinate for looking 
 
-TotLook = sum(~isnan(eyeXY(:,1))|~isnan(eyeXY(:,2)));                      % Calculate the number of samples where the participant was looking at the screen
+TotLook = sum(~isnan(eyeXY(:,1)) & ~isnan(eyeXY(:,2)));                    % Calculate the number of samples where the participant was looking at the screen
 
 TopL = sum(eyeXY(:,1)<0.5 & eyeXY(:,2)<0.5);                               % Calculate the number of samples where the participant was looking at the top left of the screen
 TopR = sum(eyeXY(:,1)>0.5 & eyeXY(:,2)<0.5);                               % Calculate the number of samples where the participant was looking at the top right of the screen
