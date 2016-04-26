e0f50a4a700dffc3e483b297f2c91d8ac82bc957
Dan <d.brady@bbk.ac.uk>
2016-04-26 15:08:49 +0100

    Updated function to work correctly

--- ET Scripts/func_VAP_labels.m
+++ ET Scripts/func_VAP_labels.m
@@ -4,29 +4,23 @@ function labels_out = func_VAP_labels(allEvents, stim_ind)
 
 movie_event = 'play_movie';
 
-movie_ind = [1; find(strncmpi(allEvents(:,3), movie_event, length(movie_event))); size(allEvents(:,3),1)];
+movie_ind = [1; find(strncmpi(allEvents(:,3), movie_event, length(movie_event)))];
 block = cell(length(movie_ind)-1,1);
-labels_out = [];
+labels_out = cell(length(stim_ind), 4);
+labels_out(:,1) = {'VAP'};
 
 for block_n = 1:size(block,1)
-    block{block_n} = stim_ind(stim_ind>movie_ind(block_n)&stim_ind<movie_ind(block_n+1));
-    out_temp = cell(length(block{block_n}),4);
-    out_temp(:,1) = {'VAP'};
-    out_temp(:,3) = {block_n};
-    if length(block{block_n}) == 10
-        out_temp(1:8,2)     = {'Familiarization'};
-        out_temp(9:10,2)    = {'Test'};
-        out_temp(1:8,4)     = num2cell(1:8);
-        out_temp(9:10,4)    = num2cell(1:2);
-    elseif length(block{block_n}) == 2
-        out_temp(:,2)       = {'Test'};
-        out_temp(:,4)       = num2cell(1:2);
-    else
-        out_temp(:,2)       = {'Unknown'};
-        out_temp(:,3:4)     = {NaN};
+    block{block_n} = find(stim_ind>movie_ind(block_n)&stim_ind<movie_ind(block_n+1));
+    if length(block{block_n}) >= 10
+        ind = find(stim_ind<movie_ind(block_n+1), 10, 'last');
+        labels_out(ind,2)     = {'Familiarization'};
+        labels_out(ind,3)     = {block_n};
+        labels_out(ind,4)     = num2cell(1:10);        
     end
-    labels_out = [labels_out; out_temp];
-    clear out_temp
+    ind = find(stim_ind<movie_ind(block_n+1), 2, 'last');
+    labels_out(ind,2)     = {'Test'};
+    labels_out(ind,3)     = {block_n};
+    labels_out(ind,4)     = num2cell(1:2);
 end
-
-labels_out = cellfun(@(x) num2str(x), labels_out, 'uni', false);
+labels_out(cellfun('isempty', labels_out)) = {'Unknown'};
+labels_out = cellfun(@num2str, labels_out, 'uni', false);
