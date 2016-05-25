8fbca05108df02543462b6a79f771730767dcabe
Dan <d.brady@bbk.ac.uk>
2016-05-25 17:58:15 +0100

    Started working out what this function does

--- ET Scripts/Fixation Parse/func_getRidofFakeSaccades.m
+++ ET Scripts/Fixation Parse/func_getRidofFakeSaccades.m
@@ -1,2 +1,8 @@
 function smoothData = func_getRidofFakeSaccades(smoothData, roughData, fixParams)
 
+% SmoothData = [Time1 Time2 X Y ValidFixes10 BelowVeloc10 Saccs10 Vel InterpolatingFlag]
+% SmoothData = [Time1 Time2 X Y ValidFixes10 BelowVeloc10 Saccs10 Vel InterpolatingFlag avgVelCrit movWinCrit binocDisparityCrit displacementCrit interpolatedThruSaccade]
+
+
+% Add more columns to the smoothData (see above)
+
