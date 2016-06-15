d597f82f6a019edbfef86c21657ec64bf5e9f9bc
Dan <d.brady@bbk.ac.uk>
2016-06-15 18:15:49 +0100

    Finds eggless path and adds functions to path list so they can be used

--- EEG Scripts/Resting State/PreprocessingScript.m
+++ EEG Scripts/Resting State/PreprocessingScript.m
@@ -4,8 +4,11 @@ orig_path = fileparts(mfilename('fullpath'));                              % Rec
 addpath(orig_path)                                                         % Add that folder to path
 cd(orig_path)                                                              % Change to that folder for creation of output file
 
-folder_search_str = 'ADDS*';                                               % Set folder search string
-file_search_str   = '*.raw';                                               % Set file search string
+res = which('eeglab.m');
+addpath(genpath([res(1:end-length('eeglab.m')) 'functions']))
+
+file_search_str   = '*.RAW';                                               % Set file search string
+savepath_str = sprintf('%s/RS/%%s/', orig_path);
 
 path  = uigetdir('','Select main data folder');                            % Ask participant to select main data folder
 if path == 0; error('Path not selected'); end                              % Throw error if no path selected
@@ -21,7 +24,17 @@ for file_n = 1:length(files)                                               % Run
     events.end      = EEG.times([EEG.event(strcmp({EEG.event.type}, 'MOVX')).latency]);
     events.length   = events.end - events.start;
     
-
+    ALLEEG = EEG;
+    
+    for event_n = 1:numel(events.ind)
+        EEG = pop_epoch(EEG, {'MOVI'}, [0 events.length(event_n)/1000], 'eventindices', events.ind(event_n));
+        if events.length(event_n)/1000 > 65
+            savepath = sprintf(savepath_str, 'Social Stim');
+        else
+            savepath = sprintf(savepath_str, 'Non-Social Stim');
+        end
+        EEG = pop_saveset(EEG, 'filename', EEG.filename, 'filepath', savepath);
+    end
     
     
 end
