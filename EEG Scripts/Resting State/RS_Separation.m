33468ab46fe408ca9f43aa0564c3b84552f21292
Dan <d.brady@bbk.ac.uk>
2016-06-16 13:27:54 +0100

    Finished script for separating the RS stimuli types

--- EEG Scripts/Resting State/RS_Separation.m
+++ EEG Scripts/Resting State/RS_Separation.m
@@ -7,34 +7,39 @@ cd(orig_path)                                                              % Cha
 res = which('eeglab.m');
 addpath(genpath([res(1:end-length('eeglab.m')) 'functions']))
 
-file_search_str   = '*.RAW';                                               % Set file search string
-savepath_str = sprintf('%s/RS/%%s/', orig_path);
-
 path  = uigetdir('','Select main data folder');                            % Ask participant to select main data folder
 if path == 0; error('Path not selected'); end                              % Throw error if no path selected
+
+file_search_str   = '*.RAW';                                               % Set file search string
+savepath_str = sprintf('%s/RS/%%s/', path);
 
 cd(path)                                                                   % Change to main data folder
 files = dir(file_search_str);                                              % Get all the ADDS files available
 
-for file_n = 1:length(files)                                               % Run through each file
+for file_n = 70:length(files)                                               % Run through each file
+    [EEG, ALLEEG, events] = deal ([]);
     EEG = pop_readegi(files(file_n).name, [],[],'auto');                   % Load file
+    EEG.setname = strtok(files(file_n).name,' .');
     
     events.ind      = [EEG.event(strcmp({EEG.event.type}, 'MOVI')).urevent];
     events.start    = EEG.times([EEG.event(strcmp({EEG.event.type}, 'MOVI')).latency]); % Find the times for 
     events.end      = EEG.times([EEG.event(strcmp({EEG.event.type}, 'MOVX')).latency]);
-    events.length   = events.end - events.start;
-    
+    try
+        events.length   = events.end - events.start;
+    catch
+        continue
+    end
     ALLEEG = EEG;
     
     for event_n = 1:numel(events.ind)
-        EEG = pop_epoch(EEG, {'MOVI'}, [0 events.length(event_n)/1000], 'eventindices', events.ind(event_n));
+        EEG = pop_epoch(EEG, {'MOVI'}, [0 events.length(event_n)/1000], 'eventindices', events.ind(event_n), 'newname', EEG.setname);
         if events.length(event_n)/1000 > 65
-            savepath = sprintf(savepath_str, 'Social Stim');
+            EEG.condition = 'Social Stim';
         else
-            savepath = sprintf(savepath_str, 'Non-Social Stim');
+            EEG.condition = 'Non-Social Stim';
         end
-        EEG = pop_saveset(EEG, 'filename', EEG.filename, 'filepath', savepath);
+        EEG = pop_saveset(EEG, 'filename', EEG.setname, 'filepath', sprintf(savepath_str, EEG.condition));
+        EEG = ALLEEG;
     end
-    
-    
+        
 end
