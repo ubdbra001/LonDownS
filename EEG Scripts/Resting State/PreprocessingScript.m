dbf22bec6203d942cc0d99fe11a82f73570805ed
Dan <d.brady@bbk.ac.uk>
2016-06-16 13:58:37 +0100

    Added Sarah's steps for pre-processing

--- EEG Scripts/Resting State/PreprocessingScript.m
+++ EEG Scripts/Resting State/PreprocessingScript.m
@@ -14,13 +14,28 @@ folder_search_str   = '*Stim';                                             % Set
 %savepath_str = sprintf('%s/RS/%%s/', path);
 
 cd(mainPath)                                                               % Change to main data folder
-folders = dir(file_search_str);                                            % Get all the ADDS files available
+folders = dir(file_search_str);                                            % Get the different stimulus types available
 
-for folder_n = 1:length(folders)
-    cd(fullfile(mainPath, folders(folder_n).name))
-    files = dir('ADDS*');
-    for file_n = 1:length(files)                                               % Run through each file
+for folder_n = 1:length(folders)                                           % Run through each folder
+    cd(fullfile(mainPath, folders(folder_n).name))                         % Change to that folder
+    files = dir('ADDS*.set');                                              % Get all .set files in that folder
+    for file_n = 1:length(files)                                           % Run through each file
+        EEG = pop_loadset('filename', files(file_n).name);                 % Load the data for the file
         
+%         montage (we use a 128 channel net)
+%         filter 0.1-30 Hz
+%         Remove ear channels (the caps don?t fit properly around the ears in our participants so we remove 3 ear electrodes from each side)
+%           - Which electrodes?
+%         I then epoched the data into either 1 or 2 sec segments
+%         Automatic artifact rejection for eye movement and blinks
+%           - Need exact criteria
+%         Visual artifact removal for movement artifacts
+%         Eye electrode removal
+%         Bad channel replacement (spherical spline interpolation)
+%           - Use automatic methods?
+%         Re-reference to average ref
+%           - Why here?
+
     end
     
 end
