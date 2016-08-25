9225082dfb01378b5d857e00d6f893e606672f8e
Dan <d.brady@bbk.ac.uk>
2016-08-25 12:06:32 +0100

    Updated to account for both versions of coding system (needs integration though)

--- PCI script/PCI_main.m
+++ PCI script/PCI_main.m
@@ -2,10 +2,13 @@
 
 %% User editable variables
 filenameFormat = 'PCI*.csv'; % General filename structure of the PCI files
-defaultDir = '/Volumes/ADDS/ADDS DATA All Subjects/Behavioural analyses/PCI/PCI datavyu coding MK RTP/PCI Datavyu export/export Ruby/actions <baby,parent>'; % Initial directory to open for dir select
+defaultDir = '/Volumes/ADDS/ADDS DATA All Subjects/Behavioural analyses/PCI/PCI datavyu coding MK RTP/PCI Datavyu export/export Ruby/'; % Initial directory to open for dir select
 
-childObjCols   = 5:7;   % The columns of the file that indicate the objects the child has picked up
-parentObjCols  = 11:13; % The columns of the file that indicate the objects the parent has picked up
+%childObjCols   = 5:7;   % The columns of the file that indicate the objects the child has picked up
+%parentObjCols  = 11:13; % The columns of the file that indicate the objects the parent has picked up
+
+childObjCols   = 5:7;
+parentObjCols  = 15:17;
 
 addpath(fileparts(mfilename('fullpath')))
 
@@ -14,12 +17,14 @@ cd(uigetdir(defaultDir))     % Select data dir and change to it
 files = dir(filenameFormat); % Find all PCI csv files
 
 for file_n = 1:length(files)
-    p_name = files(file_n).name(5:12);                                          % Generate participant name
+    name = strsplit(files(file_n).name, {'.', ' '});
+    p_name = name{2};                                          % Generate participant name
     try
-        PCI.(p_name).raw    = func_importPCI(files(file_n).name);                   % Import data from the files found
+        PCI.(p_name).raw    = func_importPCI2(files(file_n).name);                   % Import data from the files found
         PCI.(p_name).child  = func_PCIobjectstats(PCI.(p_name).raw, childObjCols);  % Produce stats for child objects
         PCI.(p_name).parent = func_PCIobjectstats(PCI.(p_name).raw, parentObjCols); % Produce stats for adult objects
     catch ME
+        disp(p_name)
         disp(getReport(ME))
         disp('Moving onto next file');
     end
